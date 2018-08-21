8755a9cbfbcdd1a4e15a169b2ec019dffa13acf6
Gastón I. Silva <givanse@gmail.com>
2018-08-20 22:55:25 -0700

    use query params

--- src/ui/components/ProjectPulse/component.ts
+++ src/ui/components/ProjectPulse/component.ts
@@ -2,4 +2,10 @@ import Component from '@glimmer/component';
 
 export default class ProjectPulse extends Component {
 
+  get projectNames() {
+    let qs = window.location.search;
+    const projectNames = qs.substring(qs.indexOf('=')+1).split(',');
+    return projectNames;
+  }
+
 }
